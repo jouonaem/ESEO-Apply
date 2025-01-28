8122285e4482ab639f944f02cdd19f5606781b2c
jouonaem <jemmanunoel@gmail.com>
2025-01-28 11:47:13 +0100

    Delete JobBoard/JobBoard/src/fr/eseo/e4/poo/projet/infralogiciel/apply directory

--- JobBoard/JobBoard/src/fr/eseo/e4/poo/projet/infralogiciel/apply/modele/Utilisateur.java
+++
@@ -1,9 +0,0 @@
-package fr.eseo.e4.poo.projet.infralogiciel.apply.modele;
-
-public abstract class Utilisateur implements InscriptionConnexion{
-	protected String identifiant;
-	protected String nom;
-	protected String prenom;
-	public String adresseMail;
-	protected String telephone;
-}
