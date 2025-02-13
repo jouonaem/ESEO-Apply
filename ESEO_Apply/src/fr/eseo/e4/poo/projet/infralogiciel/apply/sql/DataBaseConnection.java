4250b3e44eab13d6b8ab635bb00cc75dba60f2e9
jouonaem <jemmanunoel@gmail.com>
2025-02-13 18:13:48 +0100

    Mise à jour de certaines classes et du database driver managr

--- ESEO_Apply/src/fr/eseo/e4/poo/projet/infralogiciel/apply/sql/DataBaseConnection.java
+++ ESEO_Apply/src/fr/eseo/e4/poo/projet/infralogiciel/apply/sql/DataBaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConnection {
-	private static final String URL = "jdbc:mysql://localhost:3306/projet_eseo_apply";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/projet_eseo_apply";
 	private static final String USER = "root";
 	private static final String PASSWORD = "";
 	
